4d7a04da80e149aff2e2d12a2adc300cc5e4eed5
Hedwig0625 <hedwigchen060625@gmail.com>
2025-02-27 21:58:09 +0800

    Create main3.c

---
+++ main3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+  unsigned char i =200;
+  unsigned char j =220;
+  unsighed char k=0;
+  k = ((j-1) / 2)+ i;
+  printf("(%d+%d)/2 = %d\n", i ,j,k);
+  
+  return 0;
+}
