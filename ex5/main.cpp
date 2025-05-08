40db77c44d0018d53427efe597ec011dc8c6cc47
Hedwig0625 <hedwigchen060625@gmail.com>
2025-05-08 14:46:07 +0800

    Update main.cpp

--- ex5/main.cpp
+++ ex5/main.cpp
@@ -5,17 +5,11 @@ using namespace std;
 class car{
     public:
     string m_brand;
-    string m_model;
-    int m_year;
-    int m_MaxSeating;
     string m_drivemode;
 
-    car(string x,string y,int z,int s,string k){
+    car(string x,string y){
         m_brand = x;
-        m_model = y;
-        m_year = z;
-        m_MaxSeating = s;
-        m_drivemode = k;
+        m_drivemode = y;
         cout <<"constructing ";
         cout <<m_brand;
         cout <<"_car\n";
@@ -24,16 +18,12 @@ class car{
         cout <<m_drivemode;
         cout <<"\n";
     }
-    
-    int get_m_MaxSeating(){
-        return m_MaxSeating;
-    }
 };
 
 int main(){
-    car bmw_car("BMW","aa",2021,3,"Rear-wheel");
-    car audi_car("AUDI","bb",2022,4,"Front-wheel");
-    car benz_car("BENZ","cc",2023,5,"Rear-wheel");
+    car bmw_car("BMW","Rear-wheel");
+    car audi_car("AUDI","Front-wheel");
+    car benz_car("BENZ","Rear-wheel");
     
     return 0;
     
